3badc570a6f13942fee459ca7cb3735eb069cfa6
Lloyd Merithew <lloyd.merithew@knowledge2share.com>
2021-06-12 14:23:10 -0400

    Chapter 2.4.1 Challenge Solution Two Buttons with textAlignment

--- Two-Buttons Challenge/ViewController.swift
+++ Two-Buttons Challenge/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messaageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessagePressed(_ sender: Any) {
+        messaageLabel.text = "You are Awesome!"
+        messaageLabel.textColor = UIColor.blue
+        messaageLabel.textAlignment = .left
+    }
+    
+    @IBAction func showAnotherMessagePressed(_ sender: Any) {
+        messaageLabel.text = "You are Great!"
+        messaageLabel.textColor = UIColor.systemRed
+        messaageLabel.textAlignment = .right
+    }
 }
 
